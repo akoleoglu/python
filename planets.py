22808b9175d2ab4c45ba2ac4d415fe56993df936
Ahmet <ahmath18@gmail.com>
2020-07-28 21:51:41 -0700

    planets

---
+++ planets.py
@@ -0,0 +1,33 @@
+myPlanets = {
+     1 : "Mercury",
+     4 : "Mars",
+     2 : "Venus",
+     5 : "Jupiter",
+     3 : "Earth",
+     6 : "Saturn",
+     7 : "Uranus",
+     8 : "Neptune"
+}
+planetID = int(input("Please enter an ID for the planet from 1-8: "))
+if planetID == 1:
+    print(myPlanets[planetID])
+elif planetID == 2:
+    print(myPlanets[planetID])
+elif planetID ==3:
+    print(myPlanets[planetID])
+elif planetID == 4:
+    print(myPlanets[planetID])
+elif planetID ==5:
+    print(myPlanets[planetID])
+elif planetID ==6:
+    print(myPlanets[planetID])
+elif planetID ==7:
+    print(myPlanets[planetID])
+elif planetID ==8:
+    print(myPlanets[planetID])
+else:
+    print("Please enter a valid ID!!!")
+
+
+
+### Late night coding
