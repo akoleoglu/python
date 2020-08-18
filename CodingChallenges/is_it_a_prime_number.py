17fdd4cfa50560c6ac39738dd64cbc76b4af20ea
Ahmet <ahmath18@gmail.com>
2020-08-18 00:02:45 -0700

    New entry and name updates

---
+++ CodingChallenges/is_it_a_prime_number.py
@@ -0,0 +1,22 @@
+ #Write a program that takes a number from the user and prints the result to check if it is a prime number.
+
+
+num = int(input("Enter a number: "))
+prime = False
+for i in range(3,num-1):
+    if num%i == 0:
+        prime = False
+        break
+    else:
+        prime = True
+
+if num==2:
+    print("Number", num, "is a prime number.")
+   
+elif prime==False:
+     print(num,'is not a prime number.')
+else:
+    print("Number ", num, "is a prime number.")
+    
+
+
